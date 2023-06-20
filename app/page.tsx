11de340b2b87941f14b6ae18807f5596a7e21baa
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-20 15:32:55 -0300

    feat: implement app folder

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import Home from "components/Home/Home"
+import "styles/tailwind.css"
+
+export default function Web() {
+  return (
+    <>
+      <Head>
+        <meta property="og:url" content="https://next-enterprise.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Home />
+    </>
+  )
+}
