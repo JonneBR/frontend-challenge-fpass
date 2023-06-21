a168794fd5dfb15f14428429767916135b0c2fbc
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-21 09:38:32 -0300

    style: add input search

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -16,12 +16,18 @@ export default function Navbar() {
     }
   }, [])
 
-  //   const navbarClasses = `fixed top-0 left-0 right-0 transition-colors duration-300 ${
-  //     scrollPos > 0 ? "bg-gray-950" : "bg-transparent"
-  //         }`
   const navbarClasses = `fixed z-10 h-20 w-full top-0 left-0 right-0 transition-colors duration-300 ${
     scrollPos > 0 ? "bg-gray-950" : "bg-transparent"
   }`
 
-  return <nav className={navbarClasses}>{/* Navigation content goes here */}</nav>
+  return (
+    <nav className={navbarClasses}>
+      <div className="flex h-full w-full items-center justify-center ">
+        <input
+          placeholder="Favorite character"
+          className="h-10 rounded-md border-2 border-gray-700 bg-gray-800 p-4 text-white outline-none md:w-1/3"
+        />
+      </div>
+    </nav>
+  )
 }
