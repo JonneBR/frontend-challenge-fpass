3efb839c4635addc54cfe4fb4ff64f08dcef1053
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-21 12:46:02 -0300

    test: ensure fetch is called with correct values

--- core/infra/protocols/http/fetch-http-client.spec.ts
+++ core/infra/protocols/http/fetch-http-client.spec.ts
@@ -1,4 +1,4 @@
-interface HttpClient<R = any> {
+interface HttpClient<R = unknown> {
   request: (data: HttpRequest) => Promise<HttpResponse<R>>
 }
 
@@ -11,19 +11,42 @@ interface HttpRequest {
 
 type HttpMethod = "GET" | "POST" | "PUT" | "DELETE"
 
-interface HttpResponse<T> {
+interface HttpResponse<T = unknown> {
   statusCode: number
   body?: T
 }
 
 class FetchHttpClient implements HttpClient {
-  request(data: HttpRequest): Promise<any> {
-    return Promise.resolve()
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    fetch(data.url, {
+      method: data.method,
+    })
+    return Promise.resolve({ statusCode: 200 })
+  }
+}
+
+export function fetchHttpClientStub(data: any, options?: any) {
+  return function fetchStub(_url: any) {
+    return new Promise((resolve) => {
+      resolve({
+        json: () =>
+          Promise.resolve({
+            ...data,
+          }),
+        ...options,
+      })
+    })
   }
 }
 
 describe("fetch-http-client", () => {
-  it("should pass", () => {
-    expect(2).toBe(2)
+  it("Should call fetch with correct values", () => {
+    const dummy: HttpRequest = { url: "random.com", method: "GET" }
+    const sut = new FetchHttpClient()
+    global.fetch = jest.fn().mockImplementation(fetchHttpClientStub({}))
+
+    sut.request(dummy)
+
+    expect(fetch).toHaveBeenCalledWith(dummy.url, { method: dummy.method })
   })
 })
