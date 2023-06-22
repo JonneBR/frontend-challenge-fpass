f13cc3fd794a96f10e1d7da56e622e0b6b4727fe
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-22 15:20:50 -0300

    feat: add handleSubmit and nav icon

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useEffect, useState } from "react"
+import Image from "next/image"
+import { type FormEvent, useEffect, useState } from "react"
 
 export default function Navbar() {
   const [scrollPos, setScrollPos] = useState(0)
@@ -20,13 +21,30 @@ export default function Navbar() {
     scrollPos > 0 ? "bg-gray-950" : "bg-transparent"
   }`
 
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const input = document.querySelector("input")
+    if (input) {
+      const inputValue = input.value
+      window.location.href = "/search/Spider-Man"
+      console.log("Submitted value:", inputValue)
+    }
+  }
+
   return (
     <nav className={navbarClasses}>
-      <div className="flex h-full w-full items-center justify-center ">
-        <input
-          placeholder="Favorite character"
-          className="h-10 rounded-md border-2 border-gray-700 bg-gray-800 p-4 text-white outline-none md:w-1/3"
-        />
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="absolute left-0 cursor-pointer" onClick={() => (window.location.href = "/")}>
+          <Image style={{ height: 70 }} width={70} height={70} src={"/favicon.ico"} alt={"favicon"} />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="w-80">
+            <input
+              placeholder="Favorite character"
+              className="h-10 w-full rounded-md border-2 border-gray-700 bg-gray-800 p-4 text-white outline-none"
+            />
+          </div>
+        </form>
       </div>
     </nav>
   )
