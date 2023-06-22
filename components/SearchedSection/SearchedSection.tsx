b7df3ffe86f840945b50510b9dcb178c6a182436
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-22 20:51:56 -0300

    feat: add type and logic for Image

--- components/SearchedSection/SearchedSection.tsx
+++ components/SearchedSection/SearchedSection.tsx
@@ -1,6 +1,27 @@
 import Image from "next/image"
+import { Image as ImageResponse } from "core/characters/domain/character"
 
-export default function SearchedSection() {
+export interface CharacterBasic {
+  name: string
+  description: string
+  thumbnail?: ImageResponse
+}
+
+interface Props {
+  character: CharacterBasic
+}
+export default function SearchedSection(props: Props) {
+  const { character } = props
+  const imageElement = character.thumbnail && (
+    <Image
+      style={{ height: 310 }}
+      width={250}
+      height={310}
+      src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+      alt={character.name}
+    />
+  )
+
   return (
     <section
       className="rotate-0 bg-gray-900 bg-cover bg-left-top bg-no-repeat after:absolute after:-bottom-1 after:top-auto after:h-32 after:w-full after:bg-gradient-to-t after:from-gray-900"
@@ -12,18 +33,12 @@ export default function SearchedSection() {
       <div className=" flex px-4 py-8 text-center lg:ml-5 lg:py-16">
         <div className="z-10 mt-10 flex flex-col items-start justify-start ">
           <h1 className="mb-4 max-w-2xl text-left text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl">
-            Spider-Man (Peter Parker)
+            {character.name}
           </h1>
           <p className="mb-6 max-w-2xl text-left font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-            {`Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.`}
+            {character.description}
           </p>
-          <Image
-            style={{ height: 310 }}
-            width={250}
-            height={310}
-            src={"http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg"}
-            alt={"Thumbnail"}
-          />
+          {imageElement}
         </div>
       </div>
     </section>
