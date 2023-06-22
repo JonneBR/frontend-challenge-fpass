137cf2b0cbecad67d0f67421bdb2caedfb46bdc5
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-22 07:32:36 -0300

    chore: add domains

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,9 @@ const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
       { source: "/ping", destination: "/api/health" },
     ]
   },
+  images: {
+    domains: ["i.annihil.us"],
+  },
 })
 
 export default config
