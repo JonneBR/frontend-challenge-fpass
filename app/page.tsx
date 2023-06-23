3969b818508f9f9eac6bdf611249851dfed6be4e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-06-23 08:16:15 -0300

    refactor: move html content to separated file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head"
-import Home from "components/Home/Home"
+import { APRESENTATION_ITEMS } from "apresentation-items"
+import Carousel from "components/Carousel/Carousel"
+import type { Character } from "core/characters/domain/character"
 import { makeListCharactersController } from "core/main/factories/make-list-characters-controller"
+import EXPLORE from "explore-characters.json"
 import "styles/tailwind.css"
 
 export default async function Web() {
@@ -17,6 +20,11 @@ export default async function Web() {
   }
   const data = await getData()
 
+  const main = APRESENTATION_ITEMS.home.mainTitle
+  const sub = APRESENTATION_ITEMS.home.subTitle
+  const description = APRESENTATION_ITEMS.home.description
+  const explore = APRESENTATION_ITEMS.home.carousel.explore
+  const week = APRESENTATION_ITEMS.home.carousel.week
   return (
     <>
       <Head>
@@ -29,7 +37,39 @@ export default async function Web() {
         <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <Home results={data} />
+
+      <section
+        className="rotate-0 bg-gray-900 bg-cover
+        bg-center bg-no-repeat after:absolute after:-bottom-1 after:top-auto after:h-32 after:w-full after:bg-gradient-to-t after:from-gray-900"
+        style={{
+          height: "750px",
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url("../images/spider-man-wallpaper.jpg")`,
+        }}
+      >
+        <div className="flex px-4 py-8 text-center lg:py-16">
+          <div className="mt-32 place-self-center">
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl xl:text-6xl">
+              <span className="text-purple-500">{main}</span>
+              {sub}
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              {description}
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="bg-gray-900">
+        <div className="mx-auto max-w-full px-4 py-8 sm:py-16 lg:px-6 ">
+          {data ? (
+            <>
+              <Carousel title={week} characters={data} />
+              <Carousel title={explore} characters={EXPLORE as unknown as Character[]} />
+            </>
+          ) : (
+            <Carousel title={explore} characters={EXPLORE as unknown as Character[]} />
+          )}
+        </div>
+      </section>
     </>
   )
 }
---
+++ apresentation-items.tsx
@@ -0,0 +1,18 @@
+export const APRESENTATION_ITEMS = {
+  home: {
+    mainTitle: "Marvel Mania:",
+    subTitle: "Exploring the Marvel Universe and its IconicCharacters",
+    description: `Unleash your inner superhero with our captivating blog, 'Marvel Mania,' where we dive deep into the
+    extraordinary world of Marvel characters. From the mighty Avengers to the enigmatic X-Men, discover the
+    incredible powers, complex backstories, and thrilling adventures that make Marvel's characters truly
+    legendary.`,
+    carousel: {
+      explore: <h2 className="mb-12 text-4xl font-bold leading-8 text-white">Explore</h2>,
+      week: (
+        <h2 className="mb-12 text-4xl font-bold leading-8 text-white">
+          Characters of the <span className="text-purple-500">week:</span>
+        </h2>
+      ),
+    },
+  },
+}
